b3e5ace83ff651af8e0b1956c2b3eb8a6359bbf4
root <sdpkjc>
2025-02-27 02:17:33 +0000

    fix

--- train_3b_grpo.sh
+++ train_3b_grpo.sh
@@ -4,10 +4,10 @@
 # alias pip='/home/weiji/anaconda3/envs/zero/bin/pip'
 
 export N_GPUS=2
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=4,5
 ray stop --force && ray start --head --include-dashboard=True
-export BASE_MODEL="model/Qwen2.5-3B"
-export DATA_DIR="data/countdown"
+export BASE_MODEL="Qwen/Qwen2.5-3B"
+export DATA_DIR="../TinyZero/countdown_data"
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-3b-grpo
 export VLLM_ATTENTION_BACKEND=XFORMERS
